2b971e9cccec9bf6681450682bec90846bf591bb
wisnshaftler <wisnshaftler@gmail.com>
2023-09-06 21:27:53 +0530

    updated

--- config.js
+++ config.js
@@ -3,6 +3,11 @@ dotenv.config();
 
 const config = {
     bot_token: process.env.BOT_TOKEN,
+    dbhost: process.env.DB_HOST,
+    dbuser: process.env.DB_USER,
+    dbpass: process.env.DB_PASSWORD,
+    dbdatabase: process.env.DB_DATABASE,
+    
 };
 
 export {config};
---
+++ dbconnection.js
@@ -0,0 +1,20 @@
+import mysql from 'mysql2';
+import { config } from './config';
+
+const pool = new mysql.createPool({
+    host: config.dbhost,
+    user: config.dbuser,
+    database: config.dbdatabase,
+    waitForConnections: true,
+    connectionLimit: 20,
+    maxIdle: 6000,
+    queueLimit: 0
+});
+const promisePool = pool.promise();
+
+async function dbConnection(query) {
+    const [rows, fields] = await promisePool.query(query);
+    return { rows, fields, query};
+}
+
+export default dbConnection;
